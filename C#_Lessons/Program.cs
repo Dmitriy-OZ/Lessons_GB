af8e65fb0392254487b315a0e60b18811c293e12
OZ <OZ@gmail.com>
2022-08-18 21:12:52 +0300

    Программа 1

--- C#_Lessons/Program.cs
+++ C#_Lessons/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Evening");
+﻿//Задача 1
+/*
+//int number = 5;
+
+Console.Write("My number is: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Kvadrat = number*number;
+
+Console.WriteLine("Kvadrat = " + Kvadrat);
+*/
+
+
+//Задача 2
+//Console.WriteLine("Vvedi pervoe: ");
