50b96c025d17bd5284074703384d29fef7c2f778
OZ <OZ@gmail.com>
2022-08-25 17:55:18 +0300

    Первое домашнее задание по введению в программированию

--- HomeWorks/HW1/Program.cs
+++ HomeWorks/HW1/Program.cs
@@ -17,4 +17,100 @@ int result = Max(
 
 Console.WriteLine(result);
 */
-// Home work №1
+// Home work №1// Home work №1
+// Home work №1// Home work №1
+// Home work №1// Home work №1
+// Home work №1// Home work №1
+//Задание 1//Задание 1//Задание 1//Задание 1//Задание 1//Задание 1//Задание 1
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+/*
+Console.Write("Enter first number: ");
+int numberF = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numberS = Convert.ToInt32(Console.ReadLine());
+
+if (numberF > numberS)
+{
+    Console.WriteLine($"Первое число {numberF} больше второго {numberS}");
+}
+else
+{
+    Console.WriteLine($"Первое число {numberF} меньше второго {numberS}");
+}
+*/
+
+//Задание 2//Задание 2//Задание 2//Задание 2//Задание 2//Задание 2//Задание 2
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт 
+//максимальное из этих чисел.
+/*
+Console.Write("Enter first number: ");
+int numberF = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numberS = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int numberT = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = 0;
+string pn = "";
+
+if (numberF > numberS)
+{
+    maxNumber = numberF;
+    pn = "первое";
+}
+else
+{
+    maxNumber = numberS;
+    pn = "второе";
+}
+if (numberT > maxNumber)
+{
+    maxNumber = numberT;
+    pn = "третье";
+}
+
+Console.WriteLine($"Из трех чисел {numberF}, {numberS}, {numberT} {pn} число самое большое {maxNumber}");
+*/
+
+//Задание 3//Задание 3//Задание 3//Задание 3//Задание 3//Задание 3//Задание 3
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ost = number % 2;
+
+//Console.WriteLine($"Число {ost} четное!");
+
+if (ost != 0)
+{
+    Console.WriteLine($"Число {number} не четное!");
+}
+else
+{
+    Console.WriteLine($"Число {number} четное!");
+}
+*/
+
+//Задание 4//Задание 4//Задание 4//Задание 4//Задание 4//Задание 4//Задание 4
+//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Clear();
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int step = 2;
+
+int num = 0;
+
+while (num < number)
+{
+    Console.Write($"{num} ");
+   //Console.Write(" ");
+    num = num + step;
+}
+Console.Write(" \n");
+*/
