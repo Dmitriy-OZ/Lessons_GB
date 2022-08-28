e59eba553236b605829d7812d464fbf06cb72acd
OZ <OZ@gmail.com>
2022-08-28 22:24:56 +0300

    Save 280822 22:24

--- HomeWorks/HW2/Program.cs
+++ HomeWorks/HW2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("HI, World!");
+﻿// Home work 2
+/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+//              123546           3
+int FindNum(int number, int razryd)
+{
+   string numST =  ;
+   Console.WriteLine($" {number}");
+   return 0;
+}
+Console.Clear();
+Console.Write("Ведите число для разбора: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите номер разряда: ");
+int razryd = Convert.ToInt32(Console.ReadLine());
+
+FindNum(number, razryd);
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
