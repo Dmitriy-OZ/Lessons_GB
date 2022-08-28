2fb91ee4c9797c0470ebc518d986dcf28a56a071
OZ <OZ@gmail.com>
2022-08-29 01:26:55 +0300

    Доделать 3е ДЗ 290822 01:26

--- HomeWorks/HW2/Program.cs
+++ HomeWorks/HW2/Program.cs
@@ -7,6 +7,7 @@
 782 -> 8
 918 -> 1
 */
+/*
 //              123546           3
 int FindNum(int number, int razryd)
 {
@@ -41,6 +42,7 @@ else
 }
 
 //Console.WriteLine(Convert.ToInt32(Convert.ToString(c)));
+*/
 /*
 Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
@@ -50,9 +52,64 @@ else
 
 32679 -> 6
 
++ *********Решение**************
+
+*//*
+int FindNum(int number, int razryd)
+{
+    string numST = number.ToString();
+
+    char[] array = new char[numST.Length];
+    for (int i = 0; i < numST.Length; i++)
+    {
+        array[i] = numST[i];
+        //Console.WriteLine($" i = {i} - {array[i]}");
+    }
+
+    return Convert.ToInt32(Convert.ToString(array[razryd-1]));
+}
+
+Console.Clear();
+Console.Write("Ведите число для разбора: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите номер разряда: ");
+int razryd = Convert.ToInt32(Console.ReadLine());
+
+//проверка что в числе разрядов больше чем запрашивается
+string numST1 = number.ToString();
+
+if (numST1.Length >= razryd)
+{
+   Console.WriteLine($" {FindNum(number, razryd)}");
+}
+else
+{
+   Console.WriteLine($" В числе {number} нет цифры под разрядом {razryd}");
+}
+
+//Console.WriteLine(Convert.ToInt32(Convert.ToString(c)));
+*/
+/*
+
+- *********Решение**************
+
 Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 6 -> да
 7 -> да
 1 -> нет
 */
+//             0 1 2 3 4 5 6
+string[] array = {"0","0","0","0","0","1","1"};
+Console.Write("Ведите день недели(от 1 до 7): ");
+string number = Convert.ToString(Console.ReadLine());
+if (number == array[5] | number == array[6])
+{
+   Console.WriteLine($" Сегодня выходной!!!");
+}
+else
+{
+   //Console.WriteLine($"  {array.Length}");
+   //Console.WriteLine($"  {number}");
+   Console.WriteLine($" Придется еще поработать {array.Length - 2 - Convert.ToInt32(number)} денька");
+}
