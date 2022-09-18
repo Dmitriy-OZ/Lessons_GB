10d91cff1532d8cc1dc940b9d38ff3cc84ed5c99
OZ <OZ@gmail.com>
2022-09-18 12:11:31 +0300

    HW5

--- HomeWorks/HW5/Program.cs
+++ HomeWorks/HW5/Program.cs
@@ -6,48 +6,109 @@ Console.WriteLine("Hello, World!");
 //Home work 5
 
 /*
-Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите 
+программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
-*//*
-const int SIZE = 20;
-int quantity1 = 0;
-var nums = Enumerable.Range(100, SIZE).ToList();
-int[] result = new int[SIZE];
-Random rand = new Random();
-for (int i = 0; i < SIZE; i++)
-{
-    int pos = rand.Next(0, nums.Count);
-    result[i] = nums[pos];
-    nums.RemoveAt(pos);
-}
-foreach (var i in result)
-{
-    Console.Write(" " + i);
-    if (i % 2 == 0)
-    {
-    quantity1 += 1;    
-    }
-    
-}
-Console.WriteLine();
-Console.Write(" Четных элементов: " + Convert.ToString(quantity1));
-*//*
-void NewRandomArray(int SizeArray)
+*/
+/*
+int NewRandomArray(int SizeArray)
 {
     int[] array = new int[SizeArray];
     Random rand = new Random();
+    int NumberOfEvenNumbers = 0;
     for (int i = 0; i < array.Length; i++)
-        array[i] = rand.Next(); // [0 - 2^31)
+        {
+            array[i] = rand.Next(100, 999); 
+            Console.Write($" {array[i]}");
+            if (array[i]%2 == 0)
+            {
+                NumberOfEvenNumbers++;
+            }
+        }   
+    return NumberOfEvenNumbers;
 }
+
+Console.Clear();
+Console.Write("Ведите длинну массива: ");
+int numberLength = Convert.ToInt32(Console.ReadLine());
+int result = NewRandomArray(numberLength);
+
+Console.WriteLine();
+Console.Write($" The number of even elements in this array is: {result}");
+Console.WriteLine();
 */
 /*
-Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
-
-
 /*
-Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int NewRandomArray(int SizeArray)
+{
+    int[] array = new int[SizeArray];
+    Random rand = new Random();
+    int SummOfNonEvenNumbers = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(10, 50); 
+            Console.Write($" {array[i]}");
+            if (i%2 != 0)
+            {
+                SummOfNonEvenNumbers += array[i];
+            }
+        }   
+    return SummOfNonEvenNumbers;
+}
+
+Console.Clear();
+Console.Write("Ведите длинну массива: ");
+int numberLength = Convert.ToInt32(Console.ReadLine());
+int result = NewRandomArray(numberLength);
+
+Console.WriteLine();
+Console.Write($" The sum of the elements standing in odd positions in this array is: {result}");
+Console.WriteLine();
+*/
+/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+минимальным элементов массива.
 [3 7 22 2 78] -> 76
+*/
+/*
+double FindMinMax(double [] arrayD)
+{
+    int minPosition = 0;
+    int maxPosition = 0;
+    for (int i = 0; i < arrayD.Length; i++)
+    {
+        if (arrayD[minPosition] > arrayD[i]) 
+            minPosition = i;
+        else if (arrayD[maxPosition] < arrayD[i])
+            maxPosition = i;    
+    }
+    return arrayD[maxPosition] - arrayD[minPosition];
+}
+
+double [] NewRandomArray(int SizeArray)
+{ 
+    double[] array = new double[SizeArray];
+    Random rand = new Random();
+    
+    for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(rand.Next(1, 100) + rand.NextDouble()); 
+            Console.Write($" {array[i]:f2}");
+        }   
+    return array;
+}
+
+Console.Clear();
+Console.Write("Ведите длинну массива: ");
+int numberLength = Convert.ToInt32(Console.ReadLine());
+double [] arry = NewRandomArray(numberLength);
+
+Console.WriteLine();
+Console.Write($" The the difference between max and min elements in this array is: {FindMinMax(arry):f2}");
+Console.WriteLine();
 */
